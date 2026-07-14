1c541124dcd7714b911d0b63f233387d1a253dc8
vinaysharmakvs <vinaysharmasmail@gmail.com>
2026-07-14 21:50:11 +0530

    Update lead-config.js

--- lead-config.js
+++ lead-config.js
@@ -3,3 +3,17 @@ window.tivoroSheetEndpoints = {
   child: "",
   business: "",
 };
+
+window.tivoroGoogleForms = {
+  booking: {
+    endpoint: "https://docs.google.com/forms/d/e/1FAIpQLSfkYfp9uOz70g4isjfja--SmjRk2qWDJLvnrFf0Dw_lVbsJmg/formResponse",
+    pendingKey: "tivoroPendingBookingLead",
+    entries: {
+      name: "entry.167359180",
+      mobile: "entry.1283440660",
+      userType: "entry.480754389",
+      requirement: "entry.1436456331",
+      support: "entry.1887173858",
+    },
+  },
+};
