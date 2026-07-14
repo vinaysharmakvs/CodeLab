fc9ca7b5534a1e6d1fd7a786cc40f5bca10128c6
vinaysharmakvs <vinaysharmasmail@gmail.com>
2026-07-14 22:05:25 +0530

    Update lead-config.js

--- lead-config.js
+++ lead-config.js
@@ -16,4 +16,17 @@ window.tivoroGoogleForms = {
       support: "entry.1887173858",
     },
   },
+  parentCourseFinder: {
+    endpoint: "https://docs.google.com/forms/d/e/1FAIpQLScDODzug8gNpWlUfh-HzPyLxFbhAVwld4-GCQ0H8VfbsmPJYA/formResponse",
+    pendingKey: "tivoroPendingParentCourseLead",
+    entries: {
+      parentName: "entry.1304065692",
+      mobile: "entry.178984596",
+      childName: "entry.600549717",
+      stage: "entry.501160405",
+      confidence: "entry.1177116952",
+      priority: "entry.1306113525",
+      support: "entry.890482539",
+    },
+  },
 };
